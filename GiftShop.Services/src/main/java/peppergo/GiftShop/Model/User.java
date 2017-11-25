7c2fc4b5753c4eb6523edcacaeb42fac68e620a3
PepperGo <meetmark.sun@gmail.com>
2017-11-25 15:48:26 -0600

    update uesr database

--- GiftShop.Services/src/main/java/peppergo/GiftShop/Model/User.java
+++ GiftShop.Services/src/main/java/peppergo/GiftShop/Model/User.java
@@ -23,6 +23,12 @@ public class User {
     
     @Column(name="password")
     private String password ;
+    
+    @Column(name="email")
+    private String email;
+    
+    @Column(name="phonenumber")
+    private String phoneNumber;
 
     public int getUserId() {
         return userId;
@@ -46,6 +52,22 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
     
     
